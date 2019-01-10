20abd94d21b7346423df00bb2b00b10ef02cc14d
Xingyuan Ding <dingding951001@gmail.com>
2019-01-09 23:37:41 -0800

    change logo and add start-up page

---
+++ Axonomy/app/src/main/java/pro/axonomy/www/StartUpPageActivity.java
@@ -0,0 +1,29 @@
+package pro.axonomy.www;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import pro.axonomy.www.login.MobileLoginActivity;
+
+/**
+ * Created by xingyuanding on 1/9/19.
+ */
+public class StartUpPageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+                Intent mobileLoginActivity = new Intent(getBaseContext(), MobileLoginActivity.class);
+                startActivity(mobileLoginActivity);
+            }
+        }, 2000);
+    }
+
+}
