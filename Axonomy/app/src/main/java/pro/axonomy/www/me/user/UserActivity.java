cde81cca4d455040e70fc2983f0aad0b4911c31f
Xingyuan Ding <dingding951001@gmail.com>
2019-03-16 18:17:31 -0700

    load more if reach end and make textview clickable partially

---
+++ Axonomy/app/src/main/java/pro/axonomy/www/me/user/UserActivity.java
@@ -0,0 +1,20 @@
+package pro.axonomy.www.me.user;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pro.axonomy.www.R;
+
+/**
+ * Created by xingyuanding on 3/16/19.
+ */
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_user);
+    }
+
+}
--- Axonomy/app/src/main/java/pro/axonomy/www/updates/UpdatesFragment.java
+++ Axonomy/app/src/main/java/pro/axonomy/www/updates/UpdatesFragment.java
@@ -1,12 +1,19 @@
 package pro.axonomy.www.updates;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TableLayout;
@@ -17,10 +24,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import pro.axonomy.www.GetHttpUrlRequestTask;
+import pro.axonomy.www.LoadImageFromURLTask;
 import pro.axonomy.www.R;
+import pro.axonomy.www.WebImageHandler;
+import pro.axonomy.www.me.user.UserActivity;
 import pro.axonomy.www.utils.observableScrollView.ObservableScrollView;
 import pro.axonomy.www.utils.observableScrollView.ScrollViewListener;
 
@@ -97,12 +109,17 @@ public class UpdatesFragment extends Fragment implements ScrollViewListener {
             TextView updateAuthor = updateView.findViewById(R.id.update_author);
             JSONObject author = item.getJSONObject("author");
             int isOfficial = author.getInt("is_official");
+            String updateAuthorImgUrl;
             if (isOfficial == 0) {
                 updateAuthor.setText(author.getString("name"));
+                updateAuthorImgUrl = author.getString("avatar");
             } else {
                 JSONObject project = item.getJSONObject("project");
                 updateAuthor.setText(project.getString("name"));
+                updateAuthorImgUrl = project.getString("logo");
             }
+            ImageView updateAuthorImg = updateView.findViewById(R.id.update_author_img);
+            WebImageHandler.UNFINISHED_ASYNC_TASKS.put(updateAuthorImgUrl, new LoadImageFromURLTask(updateAuthorImg, updateAuthorImgUrl).execute(updateAuthorImgUrl));
 
             TextView updateTime = updateView.findViewById(R.id.update_time);
             updateTime.setText(item.getString("create_at_str"));
@@ -131,19 +148,28 @@ public class UpdatesFragment extends Fragment implements ScrollViewListener {
             String users = "";
             JSONObject likes = item.getJSONObject("likes");
             JSONArray usersArray = likes.getJSONArray("items");
-            //Todo: deal with the case when the number of likes is 0
+            List<Integer> userNameLength = new ArrayList();
             for (int j = 0; j < usersArray.length(); j++) {
                 JSONObject user = usersArray.getJSONObject(j);
                 if (j != 0) {
                     users += ", ";
                 }
-                users += user.getString("user_name");
+                String userName = user.getString("user_name");
+                users += userName;
+                userNameLength.add(userName.length());
             }
             int likesCount = likes.getInt("total_count");
             if (likesCount > usersArray.length()) {
                 users += " and " + likesCount + " others liked";
             }
-            updateLikedUsers.setText(users);
+            SpannableString likesSpannableString = new SpannableString(users);
+            int start = 0;
+            for (Integer length: userNameLength) {
+                likesSpannableString.setSpan(new MyClickableSpan(), start, start+length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                start += length + 2;
+            }
+            updateLikedUsers.setText(likesSpannableString);
+            updateLikedUsers.setMovementMethod(LinkMovementMethod.getInstance());
 
             TableLayout updateCommentTable = updateView.findViewById(R.id.update_comment_table);
             JSONObject comments = item.getJSONObject("comments");
@@ -155,15 +181,17 @@ public class UpdatesFragment extends Fragment implements ScrollViewListener {
                 TextView updateCommentContent = updateCommentView.findViewById(R.id.update_comment_content);
                 String userName = userComment.getJSONObject("author").getString("name");
                 String commentContent = userComment.getString("content");
-                updateCommentContent.setText(userName + ": " + commentContent);
+                String userCommentString = userName + ": " + commentContent;
+                SpannableString commentSpannableString = new SpannableString(userCommentString);
+                commentSpannableString.setSpan(new MyClickableSpan(), 0, userName.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                updateCommentContent.setText(commentSpannableString);
+                updateCommentContent.setMovementMethod(LinkMovementMethod.getInstance());
                 updateCommentTable.addView(updateCommentView);
             }
             int commentsCount = comments.getInt("total_count");
             if (commentsCount > userComments.length()) {
                 TableRow updateCommentRow = new TableRow(getActivity());
-                View updateCommentView = view.inflate(getActivity(), R.layout.tablerow_update_comment, updateCommentRow);
-                TextView updateCommentContent = updateCommentView.findViewById(R.id.update_comment_content);
-                updateCommentContent.setText("See More>>");
+                View updateCommentView = view.inflate(getActivity(), R.layout.tablerow_update_comment_more, updateCommentRow);
                 updateCommentTable.addView(updateCommentView);
             }
 
@@ -189,6 +217,20 @@ public class UpdatesFragment extends Fragment implements ScrollViewListener {
                 e.printStackTrace();
             }
             page += 1;
+        }
+    }
+
+    class MyClickableSpan extends ClickableSpan {
+        public void onClick(View textView) {
+            Log.i("onClick","MyClickableSpan on click");
+            Intent userIntent = new Intent(getContext(), UserActivity.class);
+            startActivity(userIntent);
+        }
+
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            ds.setColor(getResources().getColor(R.color.colorLinkText));
+            ds.setUnderlineText(false);
         }
     }
 }
